05b30e04061b05c3e7038b71a0780fac07ed007c
jay-java <jayprajapati.tops@gmail.com>
2023-07-12 08:43:15 +0530

    first commit done

---
+++ Core JAVA/src/basic/Operators.java
@@ -0,0 +1,17 @@
+package basic;
+//1.Arithmetic Operators->+,-,*,/,%
+public class Operators {
+	public static void main(String[] args) {
+		int i=10,j=3,k;
+		System.out.println(i+j);
+		System.out.println(i-j);
+		System.out.println(i*j);
+		System.out.println(i/j);
+		System.out.println("% = "+(i%j));
+		k = i+j;
+		System.out.println("k = "+k);
+		double a,r=4;
+		a = Math.PI*r*r;
+		System.out.println("area of circle = "+a);
+	}
+}
