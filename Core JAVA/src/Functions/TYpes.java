727002bc71e1315a172ec07f41b6a7b8c9a6aa7a
jay-java <jayprajapati.tops@gmail.com>
2023-07-17 09:19:26 +0530

    loop function unary short hand

--- Core JAVA/src/Functions/TYpes.java
+++ Core JAVA/src/Functions/TYpes.java
@@ -1,9 +1,9 @@
 package Functions;
+
 //function->block of code to execute same code again and again
 //types of function 
 //1.Predefine->
 //2.User define
-
 import java.util.Scanner;
 
 //category of function
@@ -11,8 +11,8 @@ import java.util.Scanner;
 //2.Without return type with parameter
 //3.With return type without parameter
 //4.With return type with parameter
-class Call{
-	public int RunFunction() {
+class Call {
+	public int RunFunction(int i, double d, String name) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter num = ");
 		int num = sc.nextInt();
@@ -24,7 +24,8 @@ public class TYpes {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		Call c = new Call();
-		int i = c.RunFunction();
+		int i = c.RunFunction(10, 32.3, "java");
+//		int i = c.RunFunction();
 		System.out.println(i);
 		System.out.println("hello main");
 	}
