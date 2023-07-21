9773b2d2c39ad8e1c9966de7d0ed26d7e8369cd8
jay-java <jayprajapati.tops@gmail.com>
2023-07-21 09:06:23 +0530

    multideimensional

--- Core JAVA/src/array/ArrayDemo.java
+++ Core JAVA/src/array/ArrayDemo.java
@@ -1,10 +1,39 @@
 package array;
 
+import java.util.Scanner;
+
 public class ArrayDemo {
 	public static void main(String[] args) {
-		int i[]= {1,2,3,4,5,6,7,8,9,10,11};
-		for(int index=0;index<=10;index++) {
-			System.out.println(i[index]);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter size of array : ");
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		for(int index=0;index<arr.length;index++) {
+			System.out.print("enter element at arr["+index+"] : ");
+			arr[index] = sc.nextInt();
 		}
+		int sum=0;
+		for(int index=0;index<arr.length;index++) {
+			sum = sum+arr[index];
+		}
+		System.out.println("sum = "+sum);
+		System.out.println("enter number to search in array : ");
+		int num = sc.nextInt();
+		int counter =0;
+		for(int index=0;index<arr.length;index++) {
+			if(num == arr[index]) {
+				counter++;
+			}
+		}
+		if(counter>0) {
+			System.out.println("yes "+num+" is exist "+counter+" times");
+		}
+		else {
+			System.out.println("no");
+		}
+//		int i[]= {1,2,3,4,5,6,7,8,9,10,11};
+//		for(int index=0;index<=10;index++) {
+//			System.out.println(i[index]);
+//		}
 	}
 }
